60f6978dae05127a37096eca78cd63bd42f6fa21
Costanza Laudisa <c.laudisa@students.uu.nl>
2020-12-02 19:03:59 +0100

    Edited Step5.py by normalizing dataset and adding GridSearchCV for all classification methods

--- Step5.py
+++ Step5.py
@@ -20,65 +20,73 @@ X_train, X_test, y_train, y_test = train_test_split(digits, labels, stratify=lab
 print("Shape of training dataset: ", X_train.shape, "   | shape of test dataset: ", X_test.shape)
 print("Shape of training labels: ", y_train.shape, "    | shape of test dataset: ", y_test.shape)
 
-# Remember to pass the same seed with random_state to all models for reproducible results across multiple calls
+# Normalize data
+train_mean = X_train.mean()
+train_std = X_train.std()
+
+
+def norm(x):
+    return (x - train_mean) / train_std
+
+
+X_train = norm(X_train)
+X_test = norm(X_test)
+
+# Util variables
 seed = 0
+k = 5
+num_thread = 8
+max_iter = 500
+verbose = 2
 
 ### MULTINOMIAL LOGIT MODEL ###
-# Define range of C values
-c_values = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100]
-model_scores = []
+# Define hyperparameters
+log_c_values = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100]
+log_params = {'C': log_c_values}
 
-# Fit models with all C values and determine best accuracy and best C value
-for c in c_values:
-    log = LogisticRegression(penalty='l1', C=c, solver='liblinear', random_state=seed)
-    log.fit(X_train, y_train)
-    log_score = log.score(X_test, y_test)
-    model_scores.append(log_score)
-    print("Multinomial logit model with C =", c, "scored", str(round(log_score * 100, 2)), "% accuracy.")
-best_log_score = max(model_scores)
-best_c_value = c_values[model_scores.index(best_log_score)]
-print("Best accuracy is ", str(round(best_log_score * 100, 2)), "% achieved with C =", best_c_value)
+# Define Grid Search
+log = LogisticRegression(penalty='l1', solver='liblinear', max_iter=max_iter, random_state=seed)
+log_cv = GridSearchCV(log, log_params, cv=k, verbose=verbose, n_jobs=num_thread)
+log_cv.fit(X_train, y_train)
 
-# Refit model with best values and plot confusion matrix
-log = LogisticRegression(penalty='l1', C=best_c_value, solver='liblinear', random_state=seed)
-log.fit(X_train, y_train)
-log_score = log.score(X_test, y_test)
-print("Model score: ", str(round(log_score * 100, 2)))
-log_pred = log.predict(X_test)
-log_cm = confusion_matrix(y_test, log_pred)
-plt.figure(figsize=(10, 7))
-sn.heatmap(log_cm, annot=True)
-plt.show()
+print_results(log_cv, 'log_results.csv')
+
+print("Best LOG estimator:", log_cv.best_estimator_)
+print("Best LOG score:", log_cv.best_score_)
+print("Best LOG params:", log_cv.best_params_)
 
 ### SUPPORT VECTOR MACHINE ###
+# Define hyperparameters
+svm_c_values = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100]
+g_values = [0.0001, 0.001, 0.01, 0.1, 0.5, 0.7, 0.9, 1.0]
+k_values = ['linear', 'poly', 'rbf', 'sigmoid']
+svm_params = {'C': svm_c_values, 'gamma': g_values, 'kernel': k_values}
+
+# Define Grid Search
+svm = SVC(max_iter=max_iter, random_state=seed)
+svm_cv = GridSearchCV(svm, svm_params, cv=k, verbose=verbose, n_jobs=num_thread)
+svm_cv.fit(X_train, y_train)
+
+print_results(svm_cv, 'svm_results.csv')
+
+print("Best SVM estimator:", svm_cv.best_estimator_)
+print("Best SVM score:", svm_cv.best_score_)
+print("Best SVM params:", svm_cv.best_params_)
 
 ### FEED FORWARD NEURAL NETWORK ###
-# Define range of hyperparameters
+# Define hyperparameters
 activations = ['logistic', 'tanh', 'relu']
 solvers = ['sgd', 'adam']
 lr_inits = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100]
-model_configs = []
+mlp_parameters = {'activation': activations, 'solver': solvers, 'learning_rate_init': lr_inits}
 
-# Fit models with all configuration and determine best one
-for act in activations:
-    for solv in solvers:
-        for lr in lr_inits:
-            mlp = MLPClassifier(activation=act, solver=solv, learning_rate_init=lr, max_iter=500, random_state=seed)
-            mlp.fit(X_train, y_train)
-            mlp_score = mlp.score(X_test, y_test)
-            model_config = {"score": mlp_score, "activation": act, "solver": solv, "init_lr": lr}
-            model_configs.append(model_config)
-            print("Neural network accuracy: ", str(round(mlp_score * 100, 2)), " | activation =", act, ", solver =", solv, ", learning_rate_init =", lr)
-best_config = max(model_configs, key=lambda x:x['score'])
-print("Best neural network configuration: ", best_config)
+# Define Grid Search
+mlp = MLPClassifier(max_iter=max_iter, random_state=seed)
+mlp_cv = GridSearchCV(mlp, mlp_parameters, cv=k, verbose=verbose, n_jobs=num_thread)
+mlp_cv.fit(X_train, y_train)
 
-# Refit model with best configuration and plot confusion matrix
-mlp = MLPClassifier(activation=best_config["activation"], solver=best_config["solver"], learning_rate_init=best_config["init_lr"], max_iter=500, random_state=seed)
-mlp.fit(X_train, y_train)
-mlp_score = mlp.score(X_test, y_test)
-print("Model score: ", str(round(mlp_score * 100, 2)))
-mlp_pred = mlp.predict(X_test)
-mlp_cm = confusion_matrix(y_test, mlp_pred)
-plt.figure(figsize=(10, 7))
-sn.heatmap(mlp_cm, annot=True)
-plt.show()
+print_results(mlp_cv, 'mlp_results.csv')
+
+print("Best MLP estimator:", mlp_cv.best_estimator_)
+print("Best MLP score:", mlp_cv.best_score_)
+print("Best MLP params:", mlp_cv.best_params_)
--- imports.py
+++ imports.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import statistics as st
 import cv2
+import csv
 
 # Plotting
 import matplotlib.pyplot as plt
@@ -20,3 +21,9 @@ from sklearn.neural_network import MLPClassifier
 # Parameter tuning
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.model_selection import train_test_split
+
+
+def print_results(grid_cv, file_name):
+    cv_results = grid_cv.cv_results_
+    scores_df = pd.DataFrame(cv_results).sort_values(by='rank_test_score')
+    scores_df.to_csv(file_name)
